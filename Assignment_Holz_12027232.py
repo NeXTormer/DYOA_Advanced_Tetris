acb1c0abb75223163662dd931c3a589e5dacc52d
Felix Holz <felix.holz@me.com>
2020-11-20 20:16:02 +0100

    fully working (for now)

--- Assignment_Holz_12027232.py
+++ Assignment_Holz_12027232.py
@@ -28,14 +28,15 @@ class Block:
         self.height = len(shape)
 
     def right_rotation(self, rotation_options):
+        if self.possible_rotations == 1: return;
         # rotate block once clockwise
-        self.rotation = (self.rotation + 1) % (self.possible_rotations-1)
+        self.rotation = (self.rotation + 1) % (self.possible_rotations)
         self.set_shape(self.game.block_list[self.name][self.rotation])
-
         if not self.game.is_block_on_valid_position(self):
             self.left_rotation(0)
 
     def left_rotation(self, rotation_options):
+        if self.possible_rotations == 1: return;
         # rotate block once counter-clockwise
         self.rotation = (self.rotation - 1)
         if self.rotation == -1:
@@ -46,12 +47,16 @@ class Block:
             self.right_rotation(0)
 
 class Game(BaseGame):
+    def __init__(self):
+        super(Game, self).__init__()
+        self.base_speed = self.speed
+        self.level = 0
+
     def run_game(self):
         print("run_game")
         self.board = self.get_empty_board()
         fall_time = time.time()
-
-        self.speed = 3
+        self.paused = False
 
         current_block = self.get_new_block()
         next_block = self.get_new_block()
@@ -67,43 +72,68 @@ class Game(BaseGame):
         # GameLoop
         while True:
             self.test_quit_game()
-            # TODO Game Logic: implement key events & move blocks (Hint: check if move is valid/block is on the Board)
             for event in pygame.event.get():
+                # TODO: Multiple key presses are processed in single loop. good or bad?
                 if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        current_block.right_rotation(0)
                     if event.key == pygame.K_e:
+                        if self.paused: continue
+                        current_block.right_rotation(0)
+                    if event.key == pygame.K_q:
+                        if self.paused: continue
                         current_block.left_rotation(0)
                     if event.key == pygame.K_LEFT:
+                        if self.paused: continue
                         if self.is_block_on_valid_position(current_block, -1, 0):
                             current_block.x -= 1
                     if event.key == pygame.K_RIGHT:
+                        if self.paused: continue
                         if self.is_block_on_valid_position(current_block, 1, 0):
                             current_block.x += 1
+                    if event.key == pygame.K_p:
+                        self.paused = not self.paused
+                    if event.key == pygame.K_DOWN:
+                        while self.gametick(current_block):
+                            pass
 
 
+            if self.paused:
+                self.show_text("Paused")
+                self.paused = False  # because show_text blocks until key pressed
 
-
-            if self.is_block_on_valid_position(current_block, 0, 1):
-                current_block.y += 1
-            else:
+            if not self.gametick(current_block):
                 self.add_block_to_board(current_block)
                 current_block = next_block
                 next_block = self.get_new_block()
 
-            print("CurrentBlock X: " + (str(current_block.x)) + ", Y: " + str(current_block.y))
+            if self.is_block_colliding_with_board(current_block):
+                self.show_text("Game Over")
+                sys.exit()  # because show_text blocks until any key is pressed
 
+            self.speed = self.base_speed + self.level
 
             # Draw after game logic
             self.display.fill(self.background)
             self.draw_game_board()
             self.draw_score()
+            self.draw_level()
             self.draw_next_block(next_block)
             if current_block != None:
                 self.draw_block(current_block)
             pygame.display.update()
             self.set_game_speed(self.speed)
             self.clock.tick(self.speed)
+
+    # moves the block by one,
+    # returns true if everything went well
+    # returns false if block is stuck
+    def gametick(self, current_block):
+        good_position = True
+        if self.is_block_on_valid_position(current_block, 0, 1):
+            current_block.y += 1
+        else:
+            good_position = False
+
+        return good_position
 
     # Check if Coordinate given is on board (returns True/False)
     def is_coordinate_on_board(self, x, y):
@@ -113,6 +143,15 @@ class Game(BaseGame):
         if y < 0 or y > (self.board_height-1):
             return False
         return True
+
+    def is_block_colliding_with_board(self, block):
+        valid_position = False
+        for i in range(0, block.height):
+            for j in range(0, block.width):
+                if block.shape[i][j] == 'x':
+                    if not self.gameboard[block.y + i][block.x + j] == '.':
+                        valid_position = True
+        return valid_position
 
     # Parameters block, x_change (any movement done in X direction), yChange (movement in Y direction)
     # Returns True if no part of the block is outside the Board or collides with another Block
@@ -133,7 +172,6 @@ class Game(BaseGame):
     # Returns True if the line is complete
     def check_line_complete(self, y_coord):
         row = self.gameboard[y_coord]
-        print(y_coord)
 
         complete = True
         for item in row:
@@ -147,12 +185,10 @@ class Game(BaseGame):
     # Returns Number of complete lines removed
     def remove_complete_line(self):
         nr_rows_removed = 0
-        print("height: " + str(self.board_height))
 
         for i in range(0, self.board_height):
             if self.check_line_complete(i):
                 # self.gameboard.pop(i)
-
 
                 for j in range(i, 0, -1):
                     self.gameboard[j] = self.gameboard[j-1]
@@ -160,6 +196,9 @@ class Game(BaseGame):
                 self.gameboard[0] = ([self.blank_color] * self.board_width)
 
                 nr_rows_removed += 1
+
+        self.calculate_new_score(nr_rows_removed, self.level)
+        return nr_rows_removed
 
     # Create a new random block
     # Returns the newly created Block Class
@@ -183,6 +222,8 @@ class Game(BaseGame):
         new_points = self.score_dictionary[lines_removed]
         new_points *= (level + 1)
         self.score += new_points
+        #print("Score: " + str(self.score))
+        self.calculate_new_level(self.score)
         # TODO Maybe call calculate_new_level?
         # Points per lines removed corresponds to the score_directory
         # Points gained: Points per line removed at once times the level modifier!
@@ -194,7 +235,7 @@ class Game(BaseGame):
         # The level generally corresponds to the score divided by 300 points.
         # 300 -> level 1; 600 -> level 2; 900 -> level 3
         # TODO increase gamespeed by 1 on level up only
-        self.score_level = math.floor(score / 300)
+        self.level = math.floor(score / 300)
 
     # set the current game speed
     def set_game_speed(self, speed):
